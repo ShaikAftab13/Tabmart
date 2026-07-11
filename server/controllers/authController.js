4a4c43ae648910a31876a9084bd654e6b2fdfc19
Aftab Shaik <aftabshaik178@gmail.com>
2026-07-11 21:58:37 +0530

    Fixed bugs

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -22,6 +22,7 @@ const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    partitioned: true,
     maxAge: 30 * 24 * 60 * 60 * 1000
 };
 
