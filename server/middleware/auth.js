01c1a9851bf0df626dd2e709f28b7001a2b8e8d2
Aftab Shaik <aftabshaik178@gmail.com>
2026-07-11 19:56:52 +0530

    Fixed server Bugs

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,8 @@ import jwt from 'jsonwebtoken';
 
 const auth = (req, res, next) => {
     try {
+        console.log("Cookies:", req.cookies);
+
         const token = req.cookies.token;
 
         if (!token) return res.status(401).json({ message: "No token provided, authorization denied" });
